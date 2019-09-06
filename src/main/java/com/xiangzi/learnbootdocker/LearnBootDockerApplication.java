cafe2c652ee3cfa2bb382e403d7af26453580f60
qianjiu <qianjiu@email.com>
2019-09-06 09:52:20 +0800

    first commit

---
+++ src/main/java/com/xiangzi/learnbootdocker/LearnBootDockerApplication.java
@@ -0,0 +1,13 @@
+package com.xiangzi.learnbootdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnBootDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnBootDockerApplication.class, args);
+    }
+
+}
